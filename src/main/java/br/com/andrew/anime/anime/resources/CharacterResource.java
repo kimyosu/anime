c78e80fde8e594943e27ba42c35ce5cfc7dbaa98
andrew <kimyosu@protonmail.com>
2025-10-20 04:32:33 -0300

    Corrigindo "Put" para "Post"

--- src/main/java/br/com/andrew/anime/anime/resources/CharacterResource.java
+++ src/main/java/br/com/andrew/anime/anime/resources/CharacterResource.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,7 +44,7 @@ public class CharacterResource {
          */
     }
 
-    @PutMapping
+    @PostMapping
     public ResponseEntity<Character> insert(Character character){
         Character obj = service.insert(character);
         URI uri = ServletUriComponentsBuilder
